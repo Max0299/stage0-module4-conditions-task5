0fa0575c50502d7e87a0e2f6b4ef4132bc35feb4
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 20:52:58 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+
+        switch (month){
+            case (1), (3), (5), (7), (8), (10), (12) -> System.out.println("number is between 1 and 5");
+            case (4), (6), (9), (11) -> System.out.println("number is between 6 and 10");
+            default -> System.out.println("28");
+        }
     }
 }
