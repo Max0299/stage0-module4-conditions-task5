76bb25d29e44bd88fd62954064fa61fbbd98de4a
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 21:06:05 +0300

    some fixes

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
 
-        if(year % 400 != 0){
+        if(year % 400 != 0 && year <2000){
             System.out.println("not leap");
         }else if (year % 4 != 0){
             System.out.println("not leap");
-        }else  if(year % 100 == 0){
+        }else  if(year % 100 == 0 && year < 2000){
             System.out.println("not leap");
         }else{
             System.out.println("leap");
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -6,7 +6,8 @@ public class SwitchDaysInMonthPrinter {
         switch (month){
             case (1), (3), (5), (7), (8), (10), (12) -> System.out.println("31");
             case (4), (6), (9), (11) -> System.out.println("30");
-            default -> System.out.println("wrong number");
+            case (2) -> System.out.println("28");
+            default -> System.out.println("wrong number!");
         }
     }
 }
